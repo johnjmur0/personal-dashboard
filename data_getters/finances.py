494fa741ea46994c3d9a90e16e0a7718180ef67e
Jack Murphy <johnjmur0@gmail.com>
2022-03-13 11:33:10 -0400

    easy to request and convert to df

--- data_getters/finances.py
+++ data_getters/finances.py
@@ -1,1 +1,13 @@
 #curl.exe -X  POST "http://127.0.0.1:8000/get_historical_data?user_name=jjm&read_cache=true&write_cache=true"
+
+import os
+import pandas as pd
+import requests
+
+method = 'get_historical_data'
+api_server_url = 'http://127.0.0.1:8000/'
+
+url = api_server_url + method
+response = requests.post(url, verify=False)
+
+ret_df = pd.DataFrame(response.json())
