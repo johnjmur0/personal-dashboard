2db478c60ad5dbb868f08362361db77b218bd5cd
Jack Murphy <johnjmur0@gmail.com>
2022-10-01 14:16:46 -0400

    add get all data

---
+++ data_getters/get_latest_data.py
@@ -0,0 +1,14 @@
+import sys
+
+# from data_getters.utils import get_latest_file, get_user_config
+from data_getters.get_finances import Finances_Processor
+from data_getters.get_exist_data import Exist_Processor
+from data_getters.get_marvin_data import Marvin_Processor
+
+if __name__ == "__main__":
+
+    user_name = sys.argv[1]
+    Exist_Processor.get_latest_data(user_name)
+    Marvin_Processor.get_latest_data(user_name)
+    Finances_Processor.get_mint_historical_data(user_name, read_cache=False)
+    Finances_Processor.get_current_accounts(user_name)
