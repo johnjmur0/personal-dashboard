6855b589c7b6ded7cdf56c1ddd841355558c9894
Jack Murphy <johnjmur0@gmail.com>
2024-02-04 20:46:56 -0500

    mm trans df proc

--- data_getters/get_monarch_data.py
+++ data_getters/get_monarch_data.py
@@ -29,8 +29,55 @@ class Monarch_API_Getter:
         transactions = await monarch_conn.get_transactions(limit=100)
         return transactions
 
+    async def get_monarch_transactions_df(self, monarch_conn) -> pd.DataFrame:
+
+        raw_transactions = await self.get_monarch_transactions(monarch_conn)
+
+        key_cols = [
+            "date",
+            "category",
+            "account",
+            "tags",
+            "amount",
+        ]
+
+        raw_df = pd.DataFrame.from_dict(raw_transactions["allTransactions"]["results"])[
+            key_cols
+        ]
+
+        select_key_map = {
+            "account": "displayName",
+            "category": "name",
+            "tags": "name",
+        }
+
+        clean_df = raw_df[["date", "amount"]]
+
+        clean_df["account"] = pd.json_normalize(raw_df["account"])[
+            select_key_map["account"]
+        ]
+        clean_df["category"] = pd.json_normalize(raw_df["category"])[
+            select_key_map["category"]
+        ]
+
+        clean_df["tags"] = pd.json_normalize(raw_df.explode("tags")["tags"])[
+            select_key_map["tags"]
+        ]
+
+        clean_df["date"] = pd.to_datetime(clean_df["date"])
+
+        return clean_df
+
     @staticmethod
     async def get_monarch_budgets(monarch_conn: MonarchMoney) -> pd.DataFrame:
 
         transactions = await monarch_conn.get_budgets()
         return transactions
+
+
+class Monarch_Processor:
+
+    @staticmethod
+    def process_monarch_transactions(transactions_dict: dict) -> pd.DataFrame:
+
+        pass
--- tests/test_data_getters.py
+++ tests/test_data_getters.py
@@ -1,6 +1,7 @@
 import asyncio
 import pytest
 import pytest_asyncio
+import pandas as pd
 import math
 
 from data_getters.utils import Data_Getter_Utils
@@ -50,10 +51,52 @@ class Test_Data_Getter_System:
 
         assert len(transactions["allTransactions"]["results"]) > 1
 
-        transactions["allTransactions"]["results"][0]["tags"] in user_config["monarch_config"]["valid_tags"]
+        transactions["allTransactions"]["results"][0]["tags"] in user_config[
+            "monarch_config"
+        ]["valid_tags"]
+
+    @pytest.mark.asyncio
+    async def test_monarch_budgets(self, monarch_conn):
+
+        budgets = await Monarch_API_Getter.get_monarch_budgets(monarch_conn)
+
+        assert isinstance(budgets, dict)
+
+        assert (
+            abs(
+                budgets["budgetData"]["monthlyAmountsByCategory"][3]["monthlyAmounts"][
+                    0
+                ]["plannedCashFlowAmount"]
+            )
+            >= 0
+        )
+
+    @pytest.mark.asyncio
+    async def test_monarch_transaction_df(self, monarch_conn):
+
+        key_cols = [
+            "date",
+            "category",
+            "account",
+            "tags",
+            "amount",
+        ]
+
+        transactions_df = await Monarch_API_Getter().get_monarch_transactions_df(
+            monarch_conn
+        )
+
+        assert isinstance(transactions_df, pd.DataFrame)
+
+        assert set(transactions_df.columns) == set(key_cols)
+
+        assert len(transactions_df) > 10
+
+        assert len(transactions_df[transactions_df["amount"].isna()]) == 0
 
 
 class Test_Data_Getter_Processors:
+
     @staticmethod
     def test_mint_transactions_processing(user_config):
         transactions_df = Mint_Processor.clean_transactions(user_config)
